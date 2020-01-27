864976d66e5b69975db4ae5f490f447d992bec97
vscilab <a@b.com>
2020-01-27 14:27:22 -0800

    Default Message

--- src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
+++ src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
@@ -2,7 +2,7 @@ package edu.vcccd.vc.csv40;
 
 public class HelloWorld {
 
-public static void main(String[] args) {
-        System.out.println("Hello, World!\n");
-        }
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
